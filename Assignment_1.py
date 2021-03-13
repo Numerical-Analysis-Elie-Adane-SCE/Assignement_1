0ad9c7dd6810189c9f7a4a784fcaa0e21ead5950
Adane <Adaneadgo1@gmail.com>
2021-03-13 15:53:39 +0200

    epsilon variable to function doc the file

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,14 +1,26 @@
-# Elie Bracha
 # Adane Adgo
+# Elie Bracha
+
+def epsilon():
+    eps = 1.0
+    while eps + 1 > 1:
+        eps /= 2
+    return eps * 2
 
 
-eps = 1.0
-while eps + 1 > 1:
-    eps /= 2
-eps *= 2
-
-print("The machine epsilon is:", eps)
-
-print((abs(3.0 * (4.0 / 3.0 - 1) - 1) - eps))
-
+# Q1:
+""" before the Correction """
+print("before the Correction")
 print((abs(3.0 * (4.0 / 3.0 - 1) - 1)))
+
+""" after the Correction """
+print((abs(3.0 * (4.0 / 3.0 - 1) - 1) - epsilon()))
+
+#  Q2 - in the pdf file.
+
+#  Q2:
+
+
+
+
+
