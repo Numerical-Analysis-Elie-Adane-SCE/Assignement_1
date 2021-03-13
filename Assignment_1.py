42564882fe43908d803efaa62942bdbd73eb4756
Elie Bracha <eliebrachapro@gmail.com>
2021-03-13 16:04:58 +0100

    Need to do Q2

--- Assignment_1.py
+++ Assignment_1.py
@@ -13,19 +13,19 @@ def epsilon():
 
 # Q1:
 """ before the Correction """
-print("\nQ1:\nBefore the Correction:", end=" ")
-print((abs(3.0 * (4.0 / 3.0 - 1) - 1)))
+print("\nQ1:\n\nBefore the Correction: abs(3.0 * (4.0 / 3.0 - 1) - 1) = " + str(abs(3.0 * (4.0 / 3.0 - 1) - 1)) + "\n")
+
 
 """ after the Correction """
-print("After the Correction:", end=" ")
-print((abs(3.0 * (4.0 / 3.0 - 1) - 1) - epsilon()))
+print("After the Correction: (abs(3.0 * (4.0 / 3.0 - 1) - 1) - epsilon()) = " + str((abs(3.0 * (4.0 / 3.0 - 1) - 1) - epsilon())))
+
 
 #  Q2 - pdf
 """ in the pdf file"""
-print("\nQ2 in the pdf file")
+print("\nQ2:\n in the pdf file in the moodle")
 
 #  Q3 - Machine procession
-print("\nQ3: \nthe Machine procession is: " + str(epsilon()))
+print("\nQ3: \nThe Machine precision is: " + str(epsilon()))
 
 
 
