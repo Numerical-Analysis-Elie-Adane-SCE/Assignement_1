4ffa7fb58f73fd40a6cc0001d653d1c2956d874b
Adane <Adaneadgo1@gmail.com>
2021-03-15 13:00:37 +0200

    more details

--- Assignment_1.py
+++ Assignment_1.py
@@ -6,9 +6,11 @@ def epsilon():
     :return: the machine precession
     """
     eps = 1.0
+    i = -1
     while eps + 1 > 1:
         eps /= 2
-    return eps * 2
+        i += 1
+    return i
 
 
 # Q1:
@@ -19,18 +21,20 @@ print("\nQ1:\n\nBefore the Correction: abs(3.0 * (4.0 / 3.0 - 1) - 1) = " + str(
 """ after the Correction """
 print("After the Correction: (abs(3.0 * (4.0 / 3.0 - 1) - 1) - epsilon()) = " + str((abs(3.0 * (4.0 / 3.0 - 1) - 1) - epsilon())))
 
-
 #  Q2 - pdf
 """ in the pdf file"""
 print("\nQ2:\n in the pdf file in the moodle")
 
 #  Q3 - Machine procession
-print("\nQ3: \nThe Machine precision is: " + str(epsilon()))
+print("\nQ3: \nThe Machine precision is: " + str(2 ** (-1*epsilon())))
+print("which is 2^(-{0})".format(epsilon()))
 
 
 
 
-#  Q3:
+
+
+
 
 
 
