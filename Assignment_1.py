167261a64169b7b0b798ab29b931e5ff62e287e1
Adane <Adaneadgo1@gmail.com>
2021-03-16 16:35:32 +0200

    final stuff

--- Assignment_1.py
+++ Assignment_1.py
@@ -1,5 +1,8 @@
 # Adane Adgo
 # Elie Bracha
+# GitHub: https://github.com/Numerical-Analysis-Elie-Adane-SCE/Assignement_1
+import math
+
 
 def epsilon():
     """
@@ -21,11 +24,10 @@ print("After the Correction: (abs(3.0 * (4.0 / 3.0 - 1) - 1) - epsilon()) = " + 
 
 #  Q2 - pdf
 """ in the pdf file"""
-print("\nQ2:\nin the pdf file in the moodle")
+print("\nQ2:\nin the Assignment_1_Adane_Adgo_Elie_Bracha_Numerical_Analysis.pdf file.")
 
 #  Q3 - Machine procession
 print("\nQ3: \nThe Machine precision is: " + str(epsilon()))
-
 
 
 
